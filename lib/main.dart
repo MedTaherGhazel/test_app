1887d65625fd0f68c0466016a9f75fe3f699e875
Med Taher Ghazel <119009624+MedTaherGhazel@users.noreply.github.com>
2023-06-25 11:16:46 +0100

    counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/ui/pages/counter.page.dart';
 import 'package:test_app/ui/pages/gallery.page.dart';
 import 'package:test_app/ui/pages/home.page.dart';
 import 'package:test_app/ui/pages/meteo.page.dart';
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
         "/home": (context) => HomePage(),
         "/meteo": (context) => MeteoPage(),
         "/gallery": (context) => GalleryPage(),
+        "/counter": (context) => CounterPage()
       },
       theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: "/home",
