42ce770e40ae0d8c7ccd5973df84b7a4fb4d220f
Med Taher Ghazel <119009624+MedTaherGhazel@users.noreply.github.com>
2023-06-25 10:53:50 +0100

    Menu

---
+++ lib/ui_widget/widgets/drawer.widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: const [
+          DrawerHeader(
+              decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                      radius: 0.7,
+                      colors: [Colors.red, Color.fromARGB(255, 0, 163, 245)])),
+              child: Center(
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage("images/hbb.jpg"),
+                ),
+              )),
+          ListTile(
+            title: Text(
+              "Home",
+              style: TextStyle(fontSize: 20),
+            ),
+            leading: Icon(
+              Icons.home,
+              color: Colors.red,
+            ),
+            trailing: Icon(
+              Icons.arrow_right,
+              color: Colors.red,
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.pushNamed(context, "/lib/ui/pages/home.page.dart");
+            },
+          ),
+          Divider(
+            height: 2,
+          ),
+          ListTile(
+            title: Text(
+              "Meteo",
+              style: TextStyle(fontSize: 20),
+            ),
+            leading: Icon(
+              Icons.ac_unit,
+              color: Colors.red,
+            ),
+            trailing: Icon(
+              Icons.arrow_right,
+              color: Colors.red,
+            ),
+          ),
+          Divider(
+            height: 2,
+          ),
+          ListTile(
+            title: Text(
+              "Gallery",
+              style: TextStyle(fontSize: 20),
+            ),
+            leading: Icon(
+              Icons.add_a_photo,
+              color: Colors.red,
+            ),
+            trailing: Icon(
+              Icons.arrow_right,
+              color: Colors.red,
+            ),
+          ),
+          Divider(
+            height: 2,
+          ),
+        ],
+      ),
+    );
+  }
+}
